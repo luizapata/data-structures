c3c77fd91d18dd92567a48bfbcae3d38aed49cf0
luizapata <lufeza95@gmail.com>
2016-06-23 20:07:19 -0700

    almoste done with tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,15 +3,35 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
+// add to tail adds the passed in argument as a value to the tail key in our list object
+
   list.addToTail = function(value){
+    //adds an object to list. tail by invoking node. 
+    list.tail = Node(value);
+    // checks if there is a head. If not, assign tail to head.
+    if(!list.head){
+      list.head = list.tail;
+    }
   };
 
+
   list.removeHead = function(){
+    // saves the value of head into a variable.
+    var former = list.head.value;
+    // moves tail to head.
+    list.head = list.tail;
+    // returns former head.
+    return former;
   };
 
   list.contains = function(target){
+    // check if the value of head or tail matches target if so return true else return false.
+    if(list.head.value === target || list.tail.value === target){
+      return true;
+    };
+    return false
   };
-
+  // return list
   return list;
 };
 
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,29 +1,139 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  // console.dir(newTree.value)
 
+
+  
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = {};  // fix me
+  newTree.counter = 0;
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
+
 };
 
-
   // your code here
-  newTree.children = null;  // fix me
-
+  // newTree.children = null;  // fix me
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+console.log(Tree.newTree)
+console.log(new Tree(value));
+  this.children[this.counter++] = new Tree(value);
 };
 
 treeMethods.contains = function(target){
-
+  var flag = false
+  _.each(this.children, function(obj, key){
+    if(obj.value === target){
+      flag = true;
+    }
+  });
+  return flag;
 };
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var Tree = function(value){
+//   var newTree = {};
+//   newTree.value = value;
+
+  
+//   // your code here
+//   newTree.children = [];  // fix me
+
+//   _.extend(newTree, treeMethods);
+
+//   return newTree;
+
+// };
+
+//   // your code here
+//   // newTree.children = null;  // fix me
+
+// var treeMethods = {};
+
+// treeMethods.addChild = function(value){
+
+//   this.children.push({value: value});
+// };
+
+// treeMethods.contains = function(target){
+//   var flag = false;
+//   _.each(this.children, function(obj){
+//     // console.log('val', object.value);
+
+//     if(obj.value === target){
+//       flag = true;
+//     }
+//     console.log(flag)
+//     return flag;
+//   });
+// };
+
+
+
+
+
+
